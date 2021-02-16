aca27ee32e786e4ed552c86ad05cf9121a621a2b
unknown <shinigamidaikou18@gmail.com>
2021-02-16 15:47:52 +0100

    reset button + player controller
    -reset button
    -player character controller(up and down)

--- Assets/Scripts/level_init.cs
+++ Assets/Scripts/level_init.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 
 public class level_init : MonoBehaviour
 {
-    // Player character
+    public static level_init instance = null ;
+
+    // Player character prefeb
     public GameObject character ;
+
+    // Player character
+    private GameObject player ;
 
     //Parent of player character possible position 
     public Transform zones ;
 
     //array of player character possible position initialized with the parent
     public Transform[] zonesTab ;
+
+    public int currentIndex;
+
+
+    void Awake()
+    {
+        if(instance == null){
+            instance = this;
+        }else if(instance != this){
+            //instance = this;          //if need to overwrite previous values
+            Destroy(gameObject);        //if need to keep previous values               (no notable differences between the two choices so far)
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -25,11 +43,14 @@ public class level_init : MonoBehaviour
                 zonesTab[i] = zones.GetChild(i);                                        //
             }                                                                           ///////////////////////////
 
-            GameObject player = Instantiate(character);
+            player = Instantiate(character);
 
-            player.transform.position = zonesTab[zonesTab.Length/2].position;           //Player character position set at the middle position (odd number)
+            currentIndex = zonesTab.Length/2;
+
+            player.transform.position = zonesTab[currentIndex].position;           //Player character position set at the middle position (odd number)
 
             Debug.Log("correctly executed");
+            Debug.Log(currentIndex);
         }else{
             Debug.Log("Char and/or zones not specified");
         }
@@ -38,6 +59,19 @@ public class level_init : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.R)){
+            ResetLevel();
+        }
+        //Debug.Log(Time.time);
+    }
+
+    public void ResetLevel()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(zones.gameObject.scene.name);
         
     }
+
+    
+
+
 }
---
+++ Assets/Scripts/player_controller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)){
+            MoveUp();
+        }
+        if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)){
+            MoveDown();
+        }
+    }
+
+    void MoveUp(){
+
+        if(level_init.instance.currentIndex > 0){
+            level_init.instance.currentIndex--;
+            this.transform.position = level_init.instance.zonesTab[level_init.instance.currentIndex].position;
+        }else{
+            Debug.Log("Can' go up further");
+        }
+    }
+
+    void MoveDown(){
+
+        if(level_init.instance.currentIndex < level_init.instance.zonesTab.Length-1){
+            level_init.instance.currentIndex++;
+            this.transform.position = level_init.instance.zonesTab[level_init.instance.currentIndex].position;
+        }else{
+            Debug.Log("Can' go down further");
+        }
+    }
+}

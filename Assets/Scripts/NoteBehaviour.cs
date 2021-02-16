7fdbca247b87a8dfa724201c3d07bbff816c9e82
yyyyj <esteban.vaissiere@insa-lyon.fr>
2021-02-16 19:26:52 +0100

    Mouvements primitifs de notes

---
+++ Assets/Scripts/NoteBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class NoteBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   if (transform.position.y > -6.5) {
+            
+            transform.position = new Vector3(transform.position.x, Convert.ToSingle(transform.position.y -  0.005 * 1), transform.position.z);
+        }
+        
+    }
+}

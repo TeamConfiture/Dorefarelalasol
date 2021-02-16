31af8b7d9e938c87baca16b828b083b013f92732
unknown <shinigamidaikou18@gmail.com>
2021-02-16 14:00:16 +0100

    debut du projet
    -Placeholder level
    -Placeholder character
    -Placeholder character position
    -Player initialisation script (level_init in background GameObject)

---
+++ Assets/Scripts/level_init.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level_init : MonoBehaviour
+{
+    // Player character
+    public GameObject character ;
+
+    //Parent of player character possible position 
+    public Transform zones ;
+
+    //array of player character possible position initialized with the parent
+    public Transform[] zonesTab ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SAN check
+        if (character != null && zones != null)
+        {
+            zonesTab = new Transform[zones.childCount];                                 ///////////////////////////
+            for (int i = 0; i < zones.childCount; i++)                                  //
+            {                                                                           //Array initialisation
+                zonesTab[i] = zones.GetChild(i);                                        //
+            }                                                                           ///////////////////////////
+
+            GameObject player = Instantiate(character);
+
+            player.transform.position = zonesTab[zonesTab.Length/2].position;           //Player character position set at the middle position (odd number)
+
+            Debug.Log("correctly executed");
+        }else{
+            Debug.Log("Char and/or zones not specified");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

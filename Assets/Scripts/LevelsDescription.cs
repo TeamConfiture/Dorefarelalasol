d016e22cbed7b9a6175572f71cd00f672b8119ab
Mathis Dano <mage.de.lombre@gmail.com>
2021-02-17 23:05:00 +0100

    First notes for the first level

---
+++ Assets/Scripts/LevelsDescription.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelsDescription : MonoBehaviour
+{
+
+    private static int dT = 526;
+    private static int sdT = 1;
+
+    /**
+     * Description of the level 1.
+     * Each line is a note.
+     * The first column correspond to the note (ex: 0 is re/fa#, 1 is fa#/la...) (from 0 to 50).
+     * The second column correspond to the the landing column (from 0 to 7).
+     * The third column correspond to the delta of time before the next note in milliseconds (from 0 to 999).
+     */
+    public static int[][] level1 = new int[][] {
+        new int[] { 0, 2, sdT }, new int[] { 9, 2, dT - sdT },
+        new int[] { 1, 2, dT },
+        new int[] { 2, 2, dT },
+        new int[] { 1, 2, dT },
+        new int[] { 0, 2, dT },
+        new int[] { 4, 2, sdT }, new int[] { 10, 6, dT - sdT },
+        new int[] { 4, 2, sdT }, new int[] { 10, 6, dT - sdT },
+        new int[] { 4, 2, sdT }, new int[] { 10, 6, dT - sdT },
+
+        new int[] { 5, 2, dT },
+        new int[] { 4, 2, sdT }, new int[] { 9, 6, dT - sdT },
+        new int[] { 0, 2, dT },
+        new int[] { 1, 2, dT },
+        new int[] { 2, 2, dT },
+        new int[] { 1, 2, dT },
+        new int[] { 0, 2, dT }, 
+        new int[] { 6, 2, sdT }, new int[] { 10, 6, dT - sdT },
+        new int[] { 7, 2, sdT }, new int[] { 12, 6, dT - sdT },
+        new int[] { 8, 2, sdT }, new int[] { 13, 6, dT - sdT },
+
+        new int[] { 14, 2, dT },
+        new int[] { 4, 2, dT },
+        new int[] { 15, 2, dT },
+        new int[] { 4, 2, dT },
+        new int[] { 14, 2, dT },
+        new int[] { 4, 2, dT },
+        new int[] { 16, 2, dT },
+        new int[] { 15, 2, dT },
+        new int[] { 18, 2, dT },
+        new int[] { 17, 2, dT },
+        new int[] { 17, 2, dT },
+        new int[] { 17, 2, dT },
+
+        new int[] { 17, 2, dT },
+        new int[] { 17, 2, dT },
+        new int[] { 17, 2, dT },
+
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

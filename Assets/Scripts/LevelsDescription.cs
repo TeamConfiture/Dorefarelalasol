b0ee215f50abe6e4d73064d4efb8475399483387
Mathis Dano <mage.de.lombre@gmail.com>
2021-02-18 16:15:07 +0100

    changed timings for level1

--- Assets/Scripts/LevelsDescription.cs
+++ Assets/Scripts/LevelsDescription.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LevelsDescription : MonoBehaviour
 {
 
-    private static int dT = 526;
+    private static int dT = 263;
     private static int sdT = 1;
 
     /**
@@ -16,66 +16,67 @@ public class LevelsDescription : MonoBehaviour
      * The third column correspond to the delta of time before the next note in milliseconds (from 0 to 999).
      */
     public static int[][] level1 = new int[][] {
-        new int[] { 0, 1, sdT }, new int[] { 9, 5, dT - sdT },
+        new int[] { 0, 1, sdT }, new int[] { 9, 5, 2 * dT - sdT },
         new int[] { 1, 2, dT },
-        new int[] { 2, 3, dT },
-        new int[] { 1, 2, dT },
+        new int[] { 2, 3, 2 * dT },
+        new int[] { 1, 2, 2 * dT },
         new int[] { 0, 1, dT },
         new int[] { 4, 2, sdT }, new int[] { 10, 6, dT - sdT },
         new int[] { 4, 2, sdT }, new int[] { 10, 6, dT - sdT },
-        new int[] { 4, 2, sdT }, new int[] { 10, 6, dT - sdT },
+        new int[] { 4, 2, sdT }, new int[] { 10, 6, 6 * dT - sdT },
 
         new int[] { 5, 2, dT },
         new int[] { 4, 2, sdT }, new int[] { 9, 5, dT - sdT },
         new int[] { 0, 1, dT },
         new int[] { 1, 2, dT },
-        new int[] { 2, 3, dT },
-        new int[] { 1, 2, dT },
+        new int[] { 2, 3, 2 * dT },
+        new int[] { 1, 2, 2 * dT },
         new int[] { 0, 2, dT },
-        new int[] { 6, 3, sdT }, new int[] { 10, 6, dT - sdT },
+        new int[] { 6, 3, sdT }, new int[] { 10, 6, 4 * dT - sdT },
         new int[] { 7, 2, sdT }, new int[] { 12, 5, dT - sdT },
-        new int[] { 8, 1, sdT }, new int[] { 13, 4, dT - sdT },
+        new int[] { 8, 1, sdT }, new int[] { 13, 4, 6 * dT - sdT },
 
-        new int[] { 14, 1, sdT }, new int[] { 25, 5, dT - sdT },
+        new int[] { 14, 1, sdT }, new int[] { 25, 5, 2 * dT - sdT },
         new int[] { 4, 2, sdT }, new int[] { 26, 6, dT - sdT },
-        new int[] { 15, 1, dT },
-        new int[] { 4, 2, sdT }, new int[] { 27, 6, dT - sdT },
-        new int[] { 14, 1, sdT }, new int[] { 28, 5, dT - sdT },
-        new int[] { 4, 2, sdT }, new int[] { 26, 6, dT - sdT },
+        new int[] { 15, 1, 2 * dT },
+        new int[] { 4, 2, sdT }, new int[] { 27, 6, 2 * dT - sdT },
+        new int[] { 14, 1, sdT }, new int[] { 28, 5, 2 * dT - sdT },
+        new int[] { 4, 2, sdT }, new int[] { 26, 6, 2 * dT - sdT },
         new int[] { 16, 1, sdT }, new int[] { 12, 5, dT - sdT },
-        new int[] { 15, 1, sdT }, new int[] { 13, 5, dT - sdT },
-        new int[] { 18, 1, dT },
+        new int[] { 15, 1, sdT }, new int[] { 13, 5, 2 * dT - sdT },
+        new int[] { 18, 1, 2 * dT },
         new int[] { 17, 0, sdT }, new int[] { 29, 4, dT - sdT },
         new int[] { 17, 0, sdT }, new int[] { 29, 4, dT - sdT },
-        new int[] { 17, 0, sdT }, new int[] { 29, 4, dT - sdT },
+        new int[] { 17, 0, sdT }, new int[] { 29, 4, 6 * dT - sdT },
 
         new int[] { 17, 2, dT },
         new int[] { 17, 2, dT },
-        new int[] { 17, 2, dT },
+        new int[] { 17, 2, 6 * dT },
 
-        new int[] { 19, 3, dT },
-        new int[] { 3, 2, dT },
-        new int[] { 20, 1, dT },
+        new int[] { 19, 3, 2 * dT },
+        new int[] { 3, 2, 2 * dT },
+        new int[] { 20, 1, 2 * dT },
         new int[] { 1, 1, dT },
-        new int[] { 2, 2, dT },
-        new int[] { 1, 1, sdT }, new int[] { 9, 5, dT - sdT },
+        new int[] { 2, 2, 2 * dT },
+        new int[] { 1, 1, sdT }, new int[] { 9, 5, 2 * dT - sdT },
         new int[] { 0, 2, sdT }, new int[] { 30, 6, dT - sdT },
 
-        new int[] { 21, 2, dT },
-        new int[] { 21, 2, dT },
-        new int[] { 21, 2, dT },
+        new int[] { 21, 2, sdT }, new int[] { 31, 7, dT - sdT },
+        new int[] { 21, 2, sdT }, new int[] { 31, 7, dT - sdT },
+        new int[] { 21, 2, sdT }, new int[] { 31, 7, 2 * dT - sdT },
 
-        new int[] { 22, 3, sdT }, new int[] { 31, 7, dT - sdT },
-        new int[] { 22, 3, sdT }, new int[] { 31, 7, dT - sdT },
-        new int[] { 22, 3, sdT }, new int[] { 31, 7, dT - sdT },
+        new int[] { 22, 3, dT },
+        new int[] { 22, 3, dT },
+        new int[] { 22, 3, 4 * dT },
 
-                                 new int[] { 32, 4, dT },
+                                 new int[] { 32, 5, dT },
+        new int[] { 0, 1, dT },
         new int[] { 1, 0, dT },
-        new int[] { 2, 1, dT },
-        new int[] { 1, 2, sdT }, new int[] { 9, 6, dT - sdT },
-        new int[] { 0, 1, sdT }, new int[] { 32, 5, dT - sdT },
+        new int[] { 2, 1, sdT }, new int[] { 9, 6, 2 * dT - sdT },
+        new int[] { 1, 2, sdT }, new int[] { 32, 5, 2 * dT - sdT },
+        new int[] { 0, 1, dT }, 
 
-        new int[] { 23, 2, dT },
+        new int[] { 23, 2, 4 * dT },
         new int[] { 24, 2, dT },
 
 

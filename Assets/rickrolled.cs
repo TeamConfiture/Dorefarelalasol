164c03e0e9b6640d2ee70e93e5051b3d79efe001
Miggy <mygntay@gmail.com>
2021-02-19 22:18:26 +0100

    rickroll

---
+++ Assets/rickrolled.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rickrolled : MonoBehaviour
+{
+    public GameObject rick;
+    bool playrick = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= 3 && !playrick)
+        {
+            playrick = true;
+            rick.SetActive(true);
+        }
+    }
+}
